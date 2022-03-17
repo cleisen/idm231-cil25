1f4129f60653aae34dbca35d9d30b977519a383e
Christina Leisen <cil25@drexel.edu>
2022-03-17 17:20:28 -0400

    organizing
    organized order of buttons and zodiac dates

--- final/app.js
+++ final/app.js
@@ -17,27 +17,22 @@ soundButtons.forEach(function (individualButtonElement) {
     audioPlayer.src = `./sounds/${audioTrackSource}`
     audioPlayer.play();
   });
-  individualButtonElement.addEventListener('userSubmit', function () {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
-    audioPlayer.src = `./sounds/${audioTrackSource}`
-    audioPlayer.play();
-  });
 });
-
+ 
 // Button Declares
 
-const originalBtn =        document.querySelector(".capricorn");
-const bakedBtn =        document.querySelector(".aquarius");
-const kettlecookedBtn = document.querySelector(".pisces");
-const sandpBtn =     document.querySelector(".aries");
-const lsaltedBtn =      document.querySelector(".taurus");
-const sandvBtn =        document.querySelector(".gemini");
-const bbqBtn =          document.querySelector(".cancer");
-const scandoBtn =       document.querySelector(".leo");
-const dillpickleBtn =   document.querySelector(".virgo");
-const candscBtn =       document.querySelector(".libra");
-const flaminhotBtn =    document.querySelector(".scorpio");
-const chilelimonBtn =   document.querySelector(".sagittarius");
+const timesBtn =        document.querySelector(".capricorn");
+const eckBtn =        document.querySelector(".aquarius");
+const halyBtn = document.querySelector(".pisces");
+const marcoBtn =     document.querySelector(".aries");
+const finoBtn =      document.querySelector(".taurus");
+const satisfyBtn =        document.querySelector(".gemini");
+const brassBtn =          document.querySelector(".cancer");
+const belyBtn =       document.querySelector(".leo");
+const noveBtn =   document.querySelector(".virgo");
+const relationBtn =       document.querySelector(".libra");
+const manofaBtn =    document.querySelector(".scorpio");
+const breeBtn =   document.querySelector(".sagittarius");
 
 
 // // Sound Declares
@@ -57,86 +52,86 @@ const chilelimonBtn =   document.querySelector(".sagittarius");
 
 // Button Functionality
 
-originalBtn.addEventListener('click', function () {
+timesBtn.addEventListener('click', function () {
   console.log('User submit button clicked');
   let AstroSign = "Cap";
   console.log('AstroSign is: ' + AstroSign);
   userPicked(AstroSign);
 });
 //
-lsaltedBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Lib";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-sandvBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Gem";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-bbqBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Leo";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-scandoBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Pis";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-dillpickleBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Can";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-candscBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Sco";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-flaminhotBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Ari";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-chilelimonBtn.addEventListener('click', function () {
-  console.log('User submit button clicked');
-  let AstroSign = "Vir";
-  console.log('AstroSign is: ' + AstroSign);
-  userPicked(AstroSign);
-});
-//
-sandpBtn.addEventListener('click', function () {
+eckBtn.addEventListener('click', function () {
   console.log('User submit button clicked');
   let AstroSign = "Aqu";
   console.log('AstroSign is: ' + AstroSign);
   userPicked(AstroSign);
 });
 //
-bakedBtn.addEventListener('click', function () {
+halyBtn.addEventListener('click', function () {
   console.log('User submit button clicked');
-  let AstroSign = "Sag";
+  let AstroSign = "Pis";
   console.log('AstroSign is: ' + AstroSign);
   userPicked(AstroSign);
 });
 //
-kettlecookedBtn.addEventListener('click', function () {
+marcoBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Ari";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+finoBtn.addEventListener('click', function () {
   console.log('User submit button clicked');
   let AstroSign = "Tau";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+satisfyBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Gem";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+brassBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Can";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+belyBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Leo";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+noveBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Vir";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+relationBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Lib";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+manofaBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Sco";
+  console.log('AstroSign is: ' + AstroSign);
+  userPicked(AstroSign);
+});
+//
+breeBtn.addEventListener('click', function () {
+  console.log('User submit button clicked');
+  let AstroSign = "Sag";
   console.log('AstroSign is: ' + AstroSign);
   userPicked(AstroSign);
 });
@@ -200,6 +195,8 @@ userSubmitObj.addEventListener('click', function () {
   userPicked(AstroSign);
 });
 
+
+
 // userPicked function
 
 function userPicked(whichOne) {
@@ -208,77 +205,90 @@ function userPicked(whichOne) {
     case 'Cap':
       zTitleObj.innerHTML = 'Times New Roman';
       zDateRangeObj.innerHTML = 'December 22nd to Januard 19th';
-      zMoreInfoObj.innerHTML = '<p>Just like this classic font, you do not want to be admired, but you do want to be respected. You are an achievement junkie that always ends up taking the lead.</p>'
+      zMoreInfoObj.innerHTML = '<p>You got Times New Roman! Just like this classic font, you do not want to be admired, but you do want to be respected. You are an achievement junkie that always ends up taking the lead.</p>'
       modal.hidden = !modal.hidden;
       break;
-      case 'Lib':
-        zTitleObj.innerHTML = 'Relation Bold';
-        zDateRangeObj.innerHTML = 'September 23rd to October 22nd';
-        zMoreInfoObj.innerHTML = '<p>Do you tend to hate being alone? Well so does this font, as you can tell by its very clingy and close together letters. This font is also similar to your traits in the way that you seem to be simultaneously extroverted and introverted.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Gem':
-        zTitleObj.innerHTML = 'Satisfy';
-        zDateRangeObj.innerHTML = 'May 21st to June 20th';
-        zMoreInfoObj.innerHTML = '<p>Sometimes a little sour&comma; sometimes a little salty&comma; but beloved by all. You&apos;re an international favorite who&apos;s kinda crazy and seems to show up at every party. You smell really bad on hot days though.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Leo':
-        zTitleObj.innerHTML = 'Bely Display';
-        zDateRangeObj.innerHTML = 'July 23rd to August 22nd';
-        zMoreInfoObj.innerHTML = '<p>You&apos;re the dad friend. Slappin&apos; those meats down on the grill with an ice cold brew in your hand&comma; talking about 80s surfer rock or telling innaproriately timed jokes... I&apos;m playing real hard into the &apos;dad&apos; thing with this one&comma; huh&quest;</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Pis':
-        zTitleObj.innerHTML = 'Halyard Display';
-        zDateRangeObj.innerHTML = 'February 19th to March 20th';
-        zMoreInfoObj.innerHTML = '<p>You&apos;re the best of both worlds. Would you eat sour cream by itself&quest; No. Onions by themselves&quest; I hope not. But mix them together and some strange alchemical reaction happens that makes them delicious. Uh&comma; you&apos;re like the personality version of that.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Can':
-        zTitleObj.innerHTML = 'P22 Brass Script Pro';
-        zDateRangeObj.innerHTML = 'June 21st to July 22nd';
-        zMoreInfoObj.innerHTML = '<p>Alright&comma; look&comma; calm down. Dill Pickle isn&apos;t THAT bad. I mean&comma; I wouldn&apos;t eat them&comma; but I know people who do&excl; And that&apos;s an honor in and of itself&comma; you know&comma; since people love to be the thing no one knows about. You&apos;re like a hipster potato&comma; which sounds like someone&apos;s Instagram username from 2010&comma; but trust me it&apos;s good. You&apos;re ahead of the curve&excl;</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Sco':
-        zTitleObj.innerHTML = 'Manofa Bold';
-        zDateRangeObj.innerHTML = 'October 23rd to November 21st';
-        zMoreInfoObj.innerHTML = '<p>You might be a little <i>cheesy</i> sometimes&comma; but your company never gets <i>sour</i>.<br>And&comma; uh&comma; that&apos;s it. That&apos;s the description.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Ari':
-        zTitleObj.innerHTML = 'Marco Bold';
-        zDateRangeObj.innerHTML = 'March 21st to April 19th';
-        zMoreInfoObj.innerHTML = '<p>The spicy member of any friend group; you&apos;ve got the heat&comma; you never back down from a challenge&comma; and you make my sensitive Irish stomach go into conniptions.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Vir':
-        zTitleObj.innerHTML = 'Novecento Sans';
-        zDateRangeObj.innerHTML = 'August 23rd to September 22nd';
-        zMoreInfoObj.innerHTML = '<p>Sour and spicy&comma; the ultimate combo. You&apos;ve got a zing to your smile and the spice of life in your eyes. You also have 160 calories per 1 oz &lpar;28g&sol;About 15 chips&rpar;. I am on the nutrition facts page.</p>'
-        modal.hidden = !modal.hidden;
-        break;
       case 'Aqu':
         zTitleObj.innerHTML = 'Eckmannpsych';
         zDateRangeObj.innerHTML = 'January 20th to February 18th';
-        zMoreInfoObj.innerHTML = '<p>My personal favorite. You can&apos;t go wrong with a good old bag of gas station salt and pepper potato chips. Reminds me of summer days heading to the park to do God knows what on a playground for five hours. I guess I&apos;d say you&apos;re a nostalgic kind of person&comma; and no I won&apos;t expand further on that.</p>'
+        zMoreInfoObj.innerHTML = '<p>She so quirkyyyy. In a good a way, I think. I&apos;m not even going to try to pronounce this for you, but that&apos;s how you know it&apos; special. This font is so eclectic, so you. You may feel like an outcast at times, but use it to your advantage! Being different makes you interesting and a fun person to be around.</p>'
         modal.hidden = !modal.hidden;
         break;
-      case 'Sag':
-        zTitleObj.innerHTML = 'Bree Oblique';
-        zDateRangeObj.innerHTML = 'November 22nd to December 21st';
-        zMoreInfoObj.innerHTML = '<p>The healthiest of all the flavors&comma; you&apos;re the type of person to go on a keto diet but not be obnoxious about it. To make strange dips with weird vegetables that always seem to taste good. You&apos;ve got style and taste&comma; but you could also use some salt.</p>'
-        modal.hidden = !modal.hidden;
-        break;
-      case 'Tau':
-        zTitleObj.innerHTML = 'Fino';
-        zDateRangeObj.innerHTML = 'April 20th to May 20th';
-        zMoreInfoObj.innerHTML = '<p>You&apos;ve got that signature crunch to your personality. What does that mean&comma; you ask&quest; I do not know.</p>'
-        modal.hidden = !modal.hidden;
-        break;
+        case 'Pis':
+          zTitleObj.innerHTML = 'Halyard Display';
+          zDateRangeObj.innerHTML = 'February 19th to March 20th';
+          zMoreInfoObj.innerHTML = '<p>Ahhh, you are Halyard Display. It is basically Helvetica, but you are not mainstream like that. I can tell that you are a mature person but can never ignore your inner child, because this font is so timeless.</p>'
+          modal.hidden = !modal.hidden;
+          break;
+          case 'Ari':
+            zTitleObj.innerHTML = 'Marco Bold';
+            zDateRangeObj.innerHTML = 'March 21st to April 19th';
+            zMoreInfoObj.innerHTML = '<p>Marco? Bold! Marco? Bold! (haha see what I did there?) This font is very bold and powerful, which is what you are. You are always determined and very ambitious in accomplishing what you want. Although life may not always go your way, I commend you for standing your ground with those thick serifs.</p>'
+            modal.hidden = !modal.hidden;
+            break;
+            case 'Tau':
+              zTitleObj.innerHTML = 'Fino';
+              zDateRangeObj.innerHTML = 'April 20th to May 20th';
+              zMoreInfoObj.innerHTML = '<p>You got Fino! Fino is very fancy, yet practical and versitile. You love a good night out, but also really enjoy being cozy. The serifs on this font show that while you like to have fun, you tend to stay grounded and when you get in the groove of something, you stay in the grove. That is an admirable quality!</p>'
+              modal.hidden = !modal.hidden;
+              break;
+              case 'Gem':
+                zTitleObj.innerHTML = 'Satisfy';
+                zDateRangeObj.innerHTML = 'May 21st to June 20th';
+                zMoreInfoObj.innerHTML = '<p>How satisfying, you got Satisfy! Some may say you tend to be a bit too faced, but I think you are wonderfully spontaneous. Your vibe is spunky, but perfect at the right times.</p>'
+                modal.hidden = !modal.hidden;
+                break;
+                case 'Can':
+                  zTitleObj.innerHTML = 'P22 Brass Script Pro';
+                  zDateRangeObj.innerHTML = 'June 21st to July 22nd';
+                  zMoreInfoObj.innerHTML = '<p>This font visually portrays you so well. The wonderful curves and strokes show how your emotional side is your strength. The way it leans forward a bit symbolizes how you are the type of person who will move on and forgive, but you always hold on just a little bit.</p>'
+                  modal.hidden = !modal.hidden;
+                  break;
+                  case 'Leo':
+                    zTitleObj.innerHTML = 'Bely Display';
+                    zDateRangeObj.innerHTML = 'July 23rd to August 22nd';
+                    zMoreInfoObj.innerHTML = '<p>Bely Display! I love this font. It&apos;s vibes are so natural and effortless, just like you! The strong serifs really portray how grounded and confident you are.</p>'
+                    modal.hidden = !modal.hidden;
+                    break;
+                    case 'Vir':
+                      zTitleObj.innerHTML = 'Novecento Sans';
+                      zDateRangeObj.innerHTML = 'August 23rd to September 22nd';
+                      zMoreInfoObj.innerHTML = '<p>Vibe check: Novecento Sans. I like this one, it is nice. No really! This font is clean and simple, which tells me that you are a very organized person at heart. However, you only emotionally invest in other people, and it is time to start thinking about yourself. See how thin the font is? That is your emotional health.</p>'
+                      modal.hidden = !modal.hidden;
+                      break;
+                      case 'Lib':
+                        zTitleObj.innerHTML = 'Relation Bold';
+                        zDateRangeObj.innerHTML = 'September 23rd to October 22nd';
+                        zMoreInfoObj.innerHTML = '<p>Relation Bold is the font for you. Do you tend to hate being alone? Well so does this font, as you can tell by its very clingy letters that cannot seem to ever let go. This font is also similar to your traits in the way that you seem to be simultaneously extroverted and introverted.</p>'
+                        modal.hidden = !modal.hidden;
+                        break;
+                        case 'Sco':
+                          zTitleObj.innerHTML = 'Manofa Bold';
+                          zDateRangeObj.innerHTML = 'October 23rd to November 21st';
+                          zMoreInfoObj.innerHTML = '<p>Oh she bold... She Manofa Bold. Just like this font, you are really cool! When people see you, they are intrigued by your vibe. However, you do have eyes that seem to stare into people&apos;s souls. While you are fun and funky, you have that bit of strong edge that is always hiding behind the curtain.</p>'
+                          modal.hidden = !modal.hidden;
+                          break;
+                          case 'Sag':
+                            zTitleObj.innerHTML = 'Bree Oblique';
+                            zDateRangeObj.innerHTML = 'November 22nd to December 21st';
+                            zMoreInfoObj.innerHTML = '<p>Bree Oblique! Yo honestly, you have the best vibe. You are kind, understanding, and have a true fun side, which I feel that this font pretty accurately shows. This font would definitely be used for some outdoorsy company&apos;s branding, because you know that the world is your playground.</p>'
+                            modal.hidden = !modal.hidden;
+                            break;
   }
 }
+
+// //sound?
+
+
+// const audioTrackSource = individualButtonElement.getAttribute("data-audio-file");
+//   // Add click event listener to each button
+//   individualButtonElement.addEventListener('userSubmitObj', function () {
+//   // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+//   audioPlayer.src = `./sounds/${audioTrackSource}`
+//   audioPlayer.play();
+//   });
+
+
 
 // Modal Controls
 
@@ -290,9 +300,9 @@ closeModalObj.addEventListener('click', function(){
   // stopAllAudio();
 });
 
-
 // Link modal element/objects from DOM tree to JS
 
 const zTitleObj = document.getElementById('zTitle');
 const zDateRangeObj = document.getElementById('zDateRange');
 const zMoreInfoObj = document.getElementById('zMoreInfo');
+
