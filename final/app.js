f0b023d472274fd9e1b2c060866a777ece78c233
Christina Leisen <cil25@drexel.edu>
2022-03-17 20:19:28 -0400

    styling of modal
    styled the modal with font images

--- final/app.js
+++ final/app.js
@@ -162,6 +162,9 @@ userSubmitObj.addEventListener('click', function () {
   const whichDayOfMonth = userBday.getUTCDate();
   console.log('whichDayOfMonth is: ' + whichDayOfMonth);
 
+  audioPlayer.src = `./sounds/${audioTrackSource}`
+    audioPlayer.play();
+
   // Display custom image based on zodiac sign
   let AstroSign = "";
 
@@ -203,73 +206,86 @@ function userPicked(whichOne) {
   console.log('userPicked called: ' + whichOne);
   switch (whichOne) {
     case 'Cap':
-      zTitleObj.innerHTML = 'Times New Roman';
+      zDisplayObj.src = 'images/fonts/times.jpg';
+      // zTitleObj.innerHTML = 'Times New Roman';
       zDateRangeObj.innerHTML = 'December 22nd to Januard 19th';
       zMoreInfoObj.innerHTML = '<p>You got Times New Roman! Just like this classic font, you do not want to be admired, but you do want to be respected. You are an achievement junkie that always ends up taking the lead.</p>'
       modal.hidden = !modal.hidden;
       break;
       case 'Aqu':
-        zTitleObj.innerHTML = 'Eckmannpsych';
+        zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+        // zTitleObj.innerHTML = 'Eckmannpsych';
         zDateRangeObj.innerHTML = 'January 20th to February 18th';
         zMoreInfoObj.innerHTML = '<p>She so quirkyyyy. In a good a way, I think. I&apos;m not even going to try to pronounce this for you, but that&apos;s how you know it&apos; special. This font is so eclectic, so you. You may feel like an outcast at times, but use it to your advantage! Being different makes you interesting and a fun person to be around.</p>'
         modal.hidden = !modal.hidden;
         break;
         case 'Pis':
-          zTitleObj.innerHTML = 'Halyard Display';
+          zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+          // zTitleObj.innerHTML = 'Halyard Display';
           zDateRangeObj.innerHTML = 'February 19th to March 20th';
           zMoreInfoObj.innerHTML = '<p>Ahhh, you are Halyard Display. It is basically Helvetica, but you are not mainstream like that. I can tell that you are a mature person but can never ignore your inner child, because this font is so timeless.</p>'
           modal.hidden = !modal.hidden;
           break;
           case 'Ari':
-            zTitleObj.innerHTML = 'Marco Bold';
+            zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+            // zTitleObj.innerHTML = 'Marco Bold';
             zDateRangeObj.innerHTML = 'March 21st to April 19th';
             zMoreInfoObj.innerHTML = '<p>Marco? Bold! Marco? Bold! (haha see what I did there?) This font is very bold and powerful, which is what you are. You are always determined and very ambitious in accomplishing what you want. Although life may not always go your way, I commend you for standing your ground with those thick serifs.</p>'
             modal.hidden = !modal.hidden;
             break;
             case 'Tau':
-              zTitleObj.innerHTML = 'Fino';
+              zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+              // zTitleObj.innerHTML = 'Fino';
               zDateRangeObj.innerHTML = 'April 20th to May 20th';
               zMoreInfoObj.innerHTML = '<p>You got Fino! Fino is very fancy, yet practical and versitile. You love a good night out, but also really enjoy being cozy. The serifs on this font show that while you like to have fun, you tend to stay grounded and when you get in the groove of something, you stay in the grove. That is an admirable quality!</p>'
               modal.hidden = !modal.hidden;
               break;
               case 'Gem':
-                zTitleObj.innerHTML = 'Satisfy';
+                zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                // zTitleObj.innerHTML = 'Satisfy';
                 zDateRangeObj.innerHTML = 'May 21st to June 20th';
                 zMoreInfoObj.innerHTML = '<p>How satisfying, you got Satisfy! Some may say you tend to be a bit too faced, but I think you are wonderfully spontaneous. Your vibe is spunky, but perfect at the right times.</p>'
                 modal.hidden = !modal.hidden;
                 break;
                 case 'Can':
-                  zTitleObj.innerHTML = 'P22 Brass Script Pro';
+                  zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                  // zTitleObj.innerHTML = 'P22 Brass Script Pro';
                   zDateRangeObj.innerHTML = 'June 21st to July 22nd';
                   zMoreInfoObj.innerHTML = '<p>This font visually portrays you so well. The wonderful curves and strokes show how your emotional side is your strength. The way it leans forward a bit symbolizes how you are the type of person who will move on and forgive, but you always hold on just a little bit.</p>'
                   modal.hidden = !modal.hidden;
                   break;
                   case 'Leo':
-                    zTitleObj.innerHTML = 'Bely Display';
+                    zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                    // zTitleObj.innerHTML = 'Bely Display';
                     zDateRangeObj.innerHTML = 'July 23rd to August 22nd';
                     zMoreInfoObj.innerHTML = '<p>Bely Display! I love this font. It&apos;s vibes are so natural and effortless, just like you! The strong serifs really portray how grounded and confident you are.</p>'
                     modal.hidden = !modal.hidden;
                     break;
                     case 'Vir':
-                      zTitleObj.innerHTML = 'Novecento Sans';
+                      zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                      // zTitleObj.innerHTML = 'Novecento Sans';
+                      zDisplayObj.src = 'images/fonts/novecento.jpg';
                       zDateRangeObj.innerHTML = 'August 23rd to September 22nd';
                       zMoreInfoObj.innerHTML = '<p>Vibe check: Novecento Sans. I like this one, it is nice. No really! This font is clean and simple, which tells me that you are a very organized person at heart. However, you only emotionally invest in other people, and it is time to start thinking about yourself. See how thin the font is? That is your emotional health.</p>'
                       modal.hidden = !modal.hidden;
                       break;
                       case 'Lib':
-                        zTitleObj.innerHTML = 'Relation Bold';
+                        zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                        // zTitleObj.innerHTML = 'Relation Bold';
                         zDateRangeObj.innerHTML = 'September 23rd to October 22nd';
                         zMoreInfoObj.innerHTML = '<p>Relation Bold is the font for you. Do you tend to hate being alone? Well so does this font, as you can tell by its very clingy letters that cannot seem to ever let go. This font is also similar to your traits in the way that you seem to be simultaneously extroverted and introverted.</p>'
                         modal.hidden = !modal.hidden;
                         break;
                         case 'Sco':
-                          zTitleObj.innerHTML = 'Manofa Bold';
+                          zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                          // zTitleObj.innerHTML = 'Manofa Bold';
                           zDateRangeObj.innerHTML = 'October 23rd to November 21st';
                           zMoreInfoObj.innerHTML = '<p>Oh she bold... She Manofa Bold. Just like this font, you are really cool! When people see you, they are intrigued by your vibe. However, you do have eyes that seem to stare into people&apos;s souls. While you are fun and funky, you have that bit of strong edge that is always hiding behind the curtain.</p>'
                           modal.hidden = !modal.hidden;
                           break;
                           case 'Sag':
-                            zTitleObj.innerHTML = 'Bree Oblique';
+                            zDisplayObj.src = 'images/fonts/Satisfy.jpg';
+                            // zTitleObj.innerHTML = 'Bree Oblique';
                             zDateRangeObj.innerHTML = 'November 22nd to December 21st';
                             zMoreInfoObj.innerHTML = '<p>Bree Oblique! Yo honestly, you have the best vibe. You are kind, understanding, and have a true fun side, which I feel that this font pretty accurately shows. This font would definitely be used for some outdoorsy company&apos;s branding, because you know that the world is your playground.</p>'
                             modal.hidden = !modal.hidden;
@@ -302,6 +318,7 @@ closeModalObj.addEventListener('click', function(){
 
 // Link modal element/objects from DOM tree to JS
 
+const zDisplayObj = document.getElementById('zDisplayImg');
 const zTitleObj = document.getElementById('zTitle');
 const zDateRangeObj = document.getElementById('zDateRange');
 const zMoreInfoObj = document.getElementById('zMoreInfo');
